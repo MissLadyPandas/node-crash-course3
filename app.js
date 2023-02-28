19a8352d9781540f8f347940186ad617ac3cdca5
MissLadyPandas <kbrumback@ivytech.edu>
2023-02-27 21:17:13 -0500

    tutora

--- app.js
+++ app.js
@@ -1,18 +1,64 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose')
+const Blog = require('./models/blog');
 
 //express app
 const app = express();
+
+//connect to mongodb
+const dbURI ='mongodb+srv://kbrumback:test1234@cluster0.kpdgknd.mongodb.net/node-tuts?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+    // .then((result) => console.log('connected to db'))
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
 
 // register view engine
 app.set('view engine', 'ejs');
 
 // listen for requests
-app.listen(3000);
+// app.listen(3000);
 
 // middleware & static files
 app.use(express.static('public'));
 app.use(morgan('dev'));
+
+// //mongoose and mongo sandbox routes
+// app.get('/add-blog', (req, res) => {
+//     const blog = new Blog({
+//         title: 'new blog2',
+//         snippet: 'about my new blog',
+//         body: 'more about my new blog'
+//     });    
+//     blog.save()
+//     .then((result) => {
+//         res.send(result)
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+// });
+
+app.get('/single-blog', (req, res) => {
+    Blog.findById('663fd5eced384ebbe707e62b4')
+    .then((result) => {
+        res.send(result)
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+});
+
+
+app.get('/all-blogs', (req, res) => {
+    Blog.find()
+    .then((result) => {
+        res.send(result);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+})
 
 
 // app.use ((req, res, next) => {
