f6d3a3aeba951a613ad2d1f1ed632158214ba392
MissLadyPandas <kbrumback@ivytech.edu>
2023-03-07 16:05:07 -0500

    modified:   app.js 	new file:   routes/blogRoutes.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose')
-const Blog = require('./models/blog');
+// const Blog = require('./models/blog');  <== removed and added to blogRoutes.js
+const blogRoutes = require('./routes/blogRoutes');
 
 //express app
 const app = express();
@@ -24,125 +25,18 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true}));
 app.use(morgan('dev'));
 
-// //mongoose and mongo sandbox routes
-// app.get('/add-blog', (req, res) => {
-//     const blog = new Blog({
-//         title: 'new blog2',
-//         snippet: 'about my new blog',
-//         body: 'more about my new blog'
-//     });    
-//     blog.save()
-//     .then((result) => {
-//         res.send(result)
-//     })
-//     .catch((err) => {
-//         console.log(err);
-//     });
-// });
-
-app.get('/single-blog', (req, res) => {
-    Blog.findById('663fd5eced384ebbe707e62b4')
-    .then((result) => {
-        res.send(result)
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-});
-
-
-app.get('/all-blogs', (req, res) => {
-    Blog.find()
-    .then((result) => {
-        res.send(result);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-})
-
-
-// app.use ((req, res, next) => {
-//     console.log('new request made:');
-//     console.log('host: ', req.hostname);
-//     console.log('path: ', req.path);
-//     console.log('method:', req.method);
-//     next();
-// });
-
-// app.use ((req, res, next) => {
-//     console.log('in the next middleware');
-//     next();
-// });
-
-// res.send('<p>home page</p>');
-app.get('/', (req, res,) => {
+// routes
+app.get('/', (req, res) => {
     res.redirect('/blogs');
-    // const blogs = [
-    //     {title: 'Yoshi finds eggs', snippet: 'lorem  iseds sdf ert sdfsf asdffg'},
-    //     {title: 'Mario finds stars', snippet: 'lorem  iseds sdf ert sdfsf asdffg'},
-    //     {title: 'How to defeat Bowser', snippet: 'lorem  iseds sdf ert sdfsf asdffg'},
-    // ]
-    // res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-    // res.send('<p>about page</p>');
+    // res.send('<p>About Page</p>');
     res.render('about', { title: 'About' });
 });
 
-// blog routes
-app.get('/blogs', (req,res) => {
-    Blog.find().sort({ createdAt: -1 })
-    .then((result) => {
-        res.render('index', { title: 'All Blogs', blogs: result } )
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-});
-
-app.post('/blogs', (req,res) => {
-    const blog = new Blog(req.body);
-
-    blog.save()
-    .then((result) => {
-        res.redirect('/blogs');
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-})
-
-
-
-app.delete('/blogs/:id', (req,res) => {
-    const id = req.params.id;
-
-    Blog.findByIdAndDelete(id)
-        .then(result => {
-            res.json({ redirect: '/blogs' })    
-        })
-        .catch(err => {
-            console.log(err);
-        })
-})
-
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create a new Blog' });
-}) 
-
-app.get('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findById(id)
-    .then(result => {
-        res.render('details', {blog: result, title: 'Blog Details'});
-    })
-    .catch(err => {
-        console.log(err);
-    })
-})
-
+//blog routes
+app.use(blogRoutes);
 
 // 404 page - this HAS to be at the bottom of the page otherwise express will match based on per list item
 app.use((req, res) => {
