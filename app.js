ced4139737c6da326f171ae3f0c971d1d97adf82
Katharine Brumback <kbrumback@ivytech.edu>
2023-03-07 19:09:45 -0500

    modified:   app.js 	new file:   controllers/blogController.js 	modified:   routes/blogRoutes.js 	renamed:    views/create.ejs -> views/blogs/create.ejs 	renamed:    views/details.ejs -> views/blogs/details.ejs 	renamed:    views/index.ejs -> views/blogs/index.ejs

--- app.js
+++ app.js
@@ -1,44 +1,43 @@
 const express = require('express');
 const morgan = require('morgan');
-const mongoose = require('mongoose')
-// const Blog = require('./models/blog');  <== removed and added to blogRoutes.js
+const mongoose = require('mongoose');
 const blogRoutes = require('./routes/blogRoutes');
 
-//express app
+// express app
 const app = express();
 
-//connect to mongodb
+// connect to mongodb & listen for requests
 const dbURI ='mongodb+srv://kbrumback:test1234@cluster0.kpdgknd.mongodb.net/node-tuts?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
-    // .then((result) => console.log('connected to db'))
-    .then((result) => app.listen(3000))
-    .catch((err) => console.log(err));
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(result => app.listen(3000))
+.catch(err => console.log(err));
 
 // register view engine
 app.set('view engine', 'ejs');
 
-// listen for requests
-// app.listen(3000);
-
 // middleware & static files
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use((req, res, next) => {
+res.locals.path = req.path;
+next();
+});
 
 // routes
 app.get('/', (req, res) => {
-    res.redirect('/blogs');
+res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
-    // res.send('<p>About Page</p>');
-    res.render('about', { title: 'About' });
+res.render('about', { title: 'About' });
 });
 
-//blog routes
-app.use(blogRoutes);
+// blog routes
+app.use('/blogs', blogRoutes);
 
-// 404 page - this HAS to be at the bottom of the page otherwise express will match based on per list item
+// 404 page
 app.use((req, res) => {
-    res.status(404).render('404', { title: '404' });
+res.status(404).render('404', { title: '404' });
 });
